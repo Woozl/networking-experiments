f9530a78f40d7ee49a6b722717792d87cdfe1497
David Glymph <Woozl@users.noreply.github.com>
2024-04-28 16:52:33 -0400

    checksum calculation

--- tcp-packet-validation/src/main.rs
+++ tcp-packet-validation/src/main.rs
@@ -1,8 +1,15 @@
 use std::{env::current_dir, fs, net::Ipv4Addr};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr_file = fs::read_to_string(current_dir()?.join("tcp_data/tcp_addrs_0.txt"))?;
-    let data_file = fs::read(current_dir()?.join("tcp_data/tcp_data_0.dat"))?;
+    for num in 0..=9 {
+        check_packet(num)?;
+    }
+    Ok(())
+}
+
+fn check_packet(num: usize) -> Result<(), Box<dyn std::error::Error>> {
+    let addr_file = fs::read_to_string(current_dir()?.join(format!("tcp_data/tcp_addrs_{num}.txt")))?;
+    let seg = fs::read(current_dir()?.join(format!("tcp_data/tcp_data_{num}.dat")))?;
 
     let [source, dest] = *addr_file
         .split_ascii_whitespace()
@@ -10,23 +17,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .collect::<Vec<_>>()
         .as_slice() else { panic!() };
 
-    // println!("Source: {source:?}, Dest: {dest:?}");
-    // println!("{data_file:?}");
-
     let mut ip_pseudo_header: Vec<u8> = vec![];
     ip_pseudo_header.extend_from_slice(&source);
     ip_pseudo_header.extend_from_slice(&dest);
     ip_pseudo_header.push(0);
     ip_pseudo_header.push(0x06);
-    ip_pseudo_header.extend_from_slice(&(data_file.len() as u16).to_be_bytes());
+    ip_pseudo_header.extend_from_slice(&(seg.len() as u16).to_be_bytes());
 
-    // println!("Psuedo header: {:?}", ip_pseudo_header);
+    // pull out checksum value from byte offset 16-17
+    let checksum = u16::from_be_bytes([seg[16], seg[17]]);
 
-    let checksum = u16::from_be_bytes([*data_file.get(16).unwrap(), *data_file.get(17).unwrap()]);
+    // duplicate tcp segment with checksum set to 0
+    let mut seg_zero_cksum = seg.clone();
+    seg_zero_cksum[16] = 0x0;
+    seg_zero_cksum[17] = 0x0;
 
-    println!("Checksum: {checksum}");
+    // pad to an even number of bytes for addition
+    if seg_zero_cksum.len() % 2 == 1 { seg_zero_cksum.push(0x0); }
+
+    // compute checksum using psuedo header + tcp header/data segment concat'd
+    // sum each 16 bit (2 byte) word using 1s compliment addition (wrap around overflow)
+    let mut combined = ip_pseudo_header.clone();
+    combined.extend(&seg_zero_cksum);
+    let computed_checksum = !(combined
+        .chunks(2)
+        .fold(0u32, |sum, word| {
+            let s = sum + u16::from_be_bytes([word[0], word[1]]) as u32;
+            (s & 0xffff) + (s >> 16)
+        }) as u16);
+
+    // if computed checksum matches checksum from header, packet is correct:
+    println!("Test {num}: {}", if checksum == computed_checksum { "PASS" } else { "FAIL" });
 
     Ok(())
-}
+} 
 
 
